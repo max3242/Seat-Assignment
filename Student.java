7c26510aa272955dd765698e6effc89a14eac9b6
max3242 <champinemd@gmail.com>
2026-04-29 14:10:17 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
-public class Student{
-  boolean back;
-  String name;
-  Arraylist<String> nextTo = new Arraylist<>;
-  public Student(String n, boolean b, String...firstLast){
-    name=n;
-    back=b;
-    for(String a: firstLast){
-      next.add(firstLast);
+public class Student {
+    boolean back;
+    String name;
+    ArrayList<String> nextTo = new ArrayList<>();
+    public Student(String n, boolean b, String... firstLast) {
+        name = n;
+        back = b;
+        for (String a : firstLast) {
+            nextTo.add(a);
+        }
     }
-  }
-  public boolean back(){
-    return this.back;
-  }
-  public String name(){
-    return this.name;
-  }
-  public Arraylist<String> nextTo(){
-    return this.nextTo;
-  }
+    public boolean isBack() {
+        return back;
+    }
+    public String getName() {
+        return name;
+    }
+    public ArrayList<String> getNextTo() {
+        return nextTo;
+    }
 }
