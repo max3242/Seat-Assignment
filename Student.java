f55efa9131f5183034270c2458e84f40c10c0c1e
max3242 <champinemd@gmail.com>
2026-04-29 13:56:16 -0700

    a

--- Student.java
+++ Student.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 public class Student{
   boolean back;
   String name;
-  Arraylist<String> nextn = new Arraylist<String>;
-  Arraylist<Student> nexts = new Arraylist<Student>;
+  Arraylist<String> nextn = new Arraylist<>;
+  Arraylist<Student> nexts = new Arraylist<>;
   public Student(String n, boolean b, String...firstLast){
     name=n;
     back=b;
@@ -17,7 +17,7 @@ public class Student{
   public String name(){
     return this.name;
   }
-  public Arraylist<String> next(){
-    return this.next;
+  public Arraylist<String> nextn(){
+    return this.nextn;
   }
 }
