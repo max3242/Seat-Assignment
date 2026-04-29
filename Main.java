d2f55ef046f0acd110aa92ab74a3dc511a5e99f5
max3242 <champinemd@gmail.com>
2026-04-29 13:55:16 -0700

    Correct ArrayList and student array initialization
    Fix ArrayList initialization and update student array dimensions.

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
 public class Main{
+  Arraylist<Student> list=new Arraylist<>;
   public Main(int numStu){
-    Student [][] a;
+    Student [][] a=new Student [5][];
+    a[0]=new Student[8];
+    a[1]=new Student[4];
+    a[1]=new Student[4];
+    a[1]=new Student[4];
+    a[1]=new Student[36-numStu];
+  }
+  order(){
+    
+  }
+  sort(){
+    
+  }
+  
+  public static void main(String[] args){
+    Main m=new Main(10);
+    //initialize all students
+    m.order();
+    System.out.println(m.sort());
   }
 }
