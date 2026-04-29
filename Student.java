cad71613a16bf637659e040a00a783d52187cf7a
max3242 <champinemd@gmail.com>
2026-04-29 10:11:45 -0700

    a
    Fixes issues with ArrayList declaration and constructor parameters.

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student{
+  boolean back;
+  String name;
+  Arraylist<Student> next = new Arraylist<Srudent>;
+  public Student(String n, boolean b){
+    name=n
+    back=b;
+  }
+  public Student (String n, boolean b, Student Arraylist<Student> a){
+    name=n;
+    back=b;
+    next=a;
+  }
+  public boolean back(){
+    return this.back;
+  }
+  public String name(){
+    return this.name;
+  }
+  public Arraylist<Student> next(){
+    return this.next;
+  }
+}
