79d7d23e539fcd2b0faeb6ca9f3ddcb113d57be2
max3242 <champinemd@gmail.com>
2026-04-29 13:38:13 -0700

    a
    Fixes issues with the Student class, including correcting ArrayList usage and constructor parameters.

--- Student.java
+++ Student.java
@@ -2,15 +2,13 @@ import java.util.ArrayList;
 public class Student{
   boolean back;
   String name;
-  Arraylist<Student> next = new Arraylist<Srudent>;
-  public Student(String n, boolean b){
-    name=n
-    back=b;
-  }
-  public Student (String n, boolean b, Student Arraylist<Student> a){
+  Arraylist<String> next = new Arraylist<String>;
+  public Student(String n, boolean b, String...firstLast){
     name=n;
     back=b;
-    next=a;
+    for(String a: firstLast){
+      next.add(firstLast);
+    }
   }
   public boolean back(){
     return this.back;
@@ -18,7 +16,7 @@ public class Student{
   public String name(){
     return this.name;
   }
-  public Arraylist<Student> next(){
+  public Arraylist<String> next(){
     return this.next;
   }
 }
